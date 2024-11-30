bfb6f4d3c592dfdd53e927237c8c04852cfb7fad
Anirudh Gupta <ganirudh118@gmail.com>
2024-11-30 18:20:54 +0530

    Update Traffic_Light_Control_Design.v

--- Traffic_Light_Control_Design.v
+++ Traffic_Light_Control_Design.v
@@ -1,15 +1,10 @@
-// Code your design here
-// Code your design here
-module Traffic_light_controller(input clk, rst, output reg [2:0] light_M1, reg [2:0] light_S, reg [2:0] light_MT, reg [2:0] light_M2);
-  
-  
 
-  
+module Traffic_light_controller(input clk, rst, output reg [2:0] light_M1, reg [2:0] light_S, reg [2:0] light_MT, reg [2:0] light_M2);
+    
   parameter s1=0, s2=1, s3=2, s4=3, s5=4, s6=5;
   reg [3:0] count;
   reg [2:0] ps;
   parameter sec7=7,sec5=5,sec2=2,sec3=3;
-  
   
   always@(posedge clk or posedge rst)
     begin
@@ -19,12 +14,8 @@ module Traffic_light_controller(input clk, rst, output reg [2:0] light_M1, reg [
           count=0;
         end
       else
-        
-        
-        
-        
-        case(ps)
-          
+            
+     case(ps)
           
           s1: if(count<sec7)
             begin
@@ -37,7 +28,6 @@ module Traffic_light_controller(input clk, rst, output reg [2:0] light_M1, reg [
               count<=0;// hence we initialize the count again
             end
           
-          
           s2: if(count<sec2)
             begin
               ps<=s2;
@@ -48,9 +38,7 @@ module Traffic_light_controller(input clk, rst, output reg [2:0] light_M1, reg [
               ps<=s3;
               count<=0;
             end
-          
-          
-          
+        
           s3: if(count<sec5)
             begin
               ps<=s3;
@@ -61,9 +49,7 @@ module Traffic_light_controller(input clk, rst, output reg [2:0] light_M1, reg [
               ps<=s4;
               count<=0;
             end
-          
-          
-          
+        
           s4: if(count<sec3)
             begin
               ps<=s4;
@@ -75,9 +61,6 @@ module Traffic_light_controller(input clk, rst, output reg [2:0] light_M1, reg [
               count<=0;
             end
           
-          
-          
-          
           s5: if(count<sec3)
             begin
               ps<=s5;
@@ -88,8 +71,6 @@ module Traffic_light_controller(input clk, rst, output reg [2:0] light_M1, reg [
               ps<=s6;
               count<=0;
             end
-          
-          
           
           s6: if(count<sec2)
             begin
@@ -105,15 +86,11 @@ module Traffic_light_controller(input clk, rst, output reg [2:0] light_M1, reg [
           default: ps<=s1;
         endcase
     end
-  
-  
-  
+
   always@(ps)
     begin
       
       case(ps)
-        
-        
         
         s1:
           begin
